3aa6d570b627e8648b3db58940253d14695ed9fa
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-03-02 21:10:18 -0500

    setup tailwind and template for ionic-react

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,20 @@
+import { IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div className="bg-red-500">
+      <div className="flex flex-row border-red-200 border-8">
+<div className="flex flex-row mx-auto mt-32 mb-32 md:hidden">
+  For mobile view
+</div>
+<div className="flex-row mx-auto mt-32 mb-32 hidden md:block">
+  For desktop view
+</div>
+</div>
+
     </div>
   );
 };
