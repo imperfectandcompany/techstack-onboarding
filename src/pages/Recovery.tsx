8e23bdb0decc49e45beddf500208a3a569202e47
sterlingwalker <68869426+sterlingwalker@users.noreply.github.com>
2022-05-10 22:35:17 -0400

    Adds UI for recovery page

--- src/pages/Recovery.tsx
+++ src/pages/Recovery.tsx
@@ -1,7 +1,16 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonLabel, IonText, IonTitle, IonInput, IonItem, IonButton, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import './Recovery.css';
 
 const Recovery: React.FC = () => {
+  const [email, setEmail] = useState<string>();
+  const [emailSent, setEmailSent] = useState<boolean>(false);
+
+  const resetPassword = () => {
+    //Do stuff
+    setEmailSent(true)
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,9 +21,33 @@ const Recovery: React.FC = () => {
           <IonTitle>Recovery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-                    
+        {!emailSent ? 
+        <IonContent>
+          <IonText color="white"> 
+            <p className="ion-text-center p-5">
+            Enter your email address to recover your account.
+            </p>
+          </IonText>
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput value={email} onIonChange={e => setEmail(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonButton className="flex font-bold"  onClick={resetPassword}>
+          Reset password
+         </IonButton>      
       </IonContent>
+        : 
+      <IonContent>
+        <IonText color="white"> 
+          <p className="ion-text-center p-5 mt-10 mb-10">
+          Instructions for resetting your password have been sent to <span className="underline">{email}</span>
+          </p>
+        </IonText>
+        <IonButton className="flex font-bold" routerLink="/signin" routerDirection="root">
+          Login
+         </IonButton>              
+    </IonContent>}
+      
     </IonPage>
   );
 };
