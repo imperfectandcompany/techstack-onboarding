b17312b6f9569e2d57ac8a9db80f97e543d4362f
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-05-20 16:48:19 -0400

    Themed email sign in page

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,43 +1,99 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonNote, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonNote, IonPage, IonProgressBar, IonTitle, IonToolbar, useIonToast } from '@ionic/react';
 import { useState } from 'react';
-import './Signup.css';
+import { useHistory } from 'react-router';
+import React from 'react';
+import styles from './SignIn.module.css'; // Import css modules stylesheet as styles
+import { eye, eyeOff, informationCircle } from 'ionicons/icons';
 
 
-const Signin: React.FC = () => {
 
+function Signin() {
   const [email, setEmail] = useState<string>();
+  const [loading, setLoading] = useState(false);
   const [password, setPassword] = useState<string>();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [attempts, setAttempts] = useState(0);
+  const [present, dismiss] = useIonToast();
+  const [passwordfocused, setPasswordFocused] = useState<boolean>(false);
 
-  const handleLogin = () => {
-    if (email) {
-      console.log("flop");
+  const history = useHistory();
+
+function togglePassword(){
+  setShowPassword(!showPassword);
+}
+
+
+
+
+
+  // handle button click of login form
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setLoading(true);
+    if(attempts >= 6){
+      setLoading(false);
+      present({
+        keyboardClose: true,
+        translucent: true,
+        duration:1000,
+        icon:informationCircle,
+        cssClass: styles.toasts,
+        buttons: [{ text: 'OK', handler: () => dismiss(), cssClass: styles.toasts }],
+        message: "Too many attempts, try later.",
+      })
+      return;
     }
-
+    setLoading(false);
   }
 
   return (
-    <IonPage>
+    <IonPage className={`${styles.bg}`} >
       <IonHeader>
-        <IonToolbar>
+      {loading ? <IonProgressBar type="indeterminate"></IonProgressBar>
+ : <IonProgressBar type="indeterminate" class="invisible"></IonProgressBar>}
+        <IonToolbar color="none"  className={`${styles.toolbar}`} >
           <IonButtons slot="start">
-            <IonBackButton defaultHref="/" />
+            <IonBackButton text="" color="white"/>
           </IonButtons>
-          <IonTitle>Sign in</IonTitle>
+          <IonTitle color="white">Sign In</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonItem>
-          <IonLabel position="floating">Email</IonLabel>
-          <IonInput value={email} onIonChange={e => setEmail(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Password</IonLabel>
-          <IonInput value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
-          <IonNote slot='error'>fwfew</IonNote>
-        </IonItem>
-        <IonButton className="flex font-bold" routerLink="/timeline" routerDirection="forward" >
-                New account
-            </IonButton>    
+      <IonContent fullscreen>
+        <IonList className={`${styles.transparentbg}`}>
+          <IonListHeader lines="full" >
+            <IonLabel class="ion-text-center" color="white">
+            Welcome back!
+
+            </IonLabel>
+          </IonListHeader>
+          <div className="flex flex-col">
+            <form id="login" className="flex flex-col"  onSubmit={(e) => {
+                              handleLogin(e)
+                            }}>
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput autocomplete="email" clearInput={true} inputmode="email" enterkeyhint="next"  type="email" value={email} onIonChange={e => setEmail(e.detail.value!)} required
+></IonInput>
+          </IonItem>
+          <IonItem className="items-end">
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput onIonFocus={()=>setPasswordFocused(true)} onIonBlur={()=>setPasswordFocused(false)} id="passwordEnter" class="passwordEnter" autocomplete="current-password" clearOnEdit={true} inputmode="text" enterkeyhint="go" type={showPassword ? 'text': 'password'} value={password} onIonChange={e => setPassword(e.detail.value!)} required
+></IonInput>
+{passwordfocused ? 
+<IonIcon slot="end" className="cursor-pointer select-none animate__animated animate__fadeIn focus:select-none hover:text-gray-200 focus:text-gray-600 focus:text-opacity-50" onClick={()=>togglePassword()} icon={showPassword ? eyeOff : eye}/>
+: null}
+          </IonItem>
+          <input type="submit" hidden className={`${styles['submit-enter']}`} />
+            <IonButton type="submit" size="large"
+            className="flex-none font-bold transition select-none ion-margin-top focus:select-none hover:text-gray-100 focus:text-gray-300 focus:text-opacity-50" color={loading ? "pprimary":"white"} fill="clear">     
+Continue
+</IonButton>
+</form>
+
+            <div className="mt-8 text-lg font-medium text-center text-white">Forgot password</div>
+          </div>
+        </IonList>
       </IonContent>
     </IonPage>
   );
