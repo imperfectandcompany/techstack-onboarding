42035059e9c783357ad1798a34c5009664bb08a6
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-05-20 21:57:17 -0400

    Small styling fixes
    up next: theme recovery page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,5 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonNote, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonNote, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { eyeOff, eye } from 'ionicons/icons';
 import { useState } from 'react';
 import styles from './SignIn.module.css'; // Import css modules stylesheet as styles
 
@@ -8,6 +9,13 @@ const Signup: React.FC = () => {
 
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [passwordfocused, setPasswordFocused] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  function togglePassword(){
+    setShowPassword(!showPassword);
+  }
+  
 
   function handleSubmit(){
     console.log({email});
@@ -30,16 +38,21 @@ const Signup: React.FC = () => {
             What's good!
             </IonLabel>
           </IonListHeader>
-          <div className="flex flex-col m-10">
+          <div className="flex flex-col ">
             <div className="mb-8 text-lg font-medium text-center text-white">We’re so excited to have you here! Creating an account is the first step.</div>
-            <IonItem className="flex" color="none">
-              <IonLabel position="floating">Email</IonLabel>
-              <IonInput type="email" value={email}  onIonChange={e => setEmail(e.detail.value!)}></IonInput>
-            </IonItem>
-            <IonItem className="flex">
-              <IonLabel position="floating">Password</IonLabel>
-              <IonInput type="password" color="white" value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
-            </IonItem>
+            <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput autocomplete="email" clearInput={true} inputmode="email" enterkeyhint="next"  type="email" value={email} onIonChange={e => setEmail(e.detail.value!)} required
+></IonInput>
+          </IonItem>
+          <IonItem className="items-end">
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput onIonFocus={()=>setPasswordFocused(true)} onIonBlur={()=>setPasswordFocused(false)} id="passwordEnter" class="passwordEnter" autocomplete="current-password" clearOnEdit={true} inputmode="text" enterkeyhint="go" type={showPassword ? 'text': 'password'} value={password} onIonChange={e => setPassword(e.detail.value!)} required
+></IonInput>
+{passwordfocused ? 
+<IonIcon slot="end" className="cursor-pointer select-none animate__animated animate__fadeIn focus:select-none hover:text-gray-200 focus:text-gray-600 focus:text-opacity-50" onClick={()=>togglePassword()} icon={showPassword ? eyeOff : eye}/>
+: null}
+          </IonItem>
             <div className="flex justify-center mt-6">
             <IonButton expand="block" size="large"
                             onClick={() => {
