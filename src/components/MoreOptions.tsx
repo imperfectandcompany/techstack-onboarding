093d5f95320bfb7590a47682ee5e3b50d683c821
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-06-11 16:17:31 -0400

    Finished feature and decoupled slideMenu from MoreOptions button
    Added notes on ExploreContainer.tsx (which should be renamed later). Removed accidental marketing.tsx component. added postid, likes, and username to PostObject. Moved data values and functions from children components to parent component for using the same values within multiple other components. Fixed key per item in list react warning. Passed username into avatar modal component, setup isOwner functionality with mock functions. Passed likes into likebutton. converted post itself to IonItem with plans for future implementation leveraging IonItems.

--- src/components/MoreOptions.tsx
+++ src/components/MoreOptions.tsx
@@ -1,69 +1,60 @@
-import { IonButton, IonContent, IonIcon, IonItem, IonLabel, IonList, IonModal, IonPopover } from "@ionic/react";
+import { GestureDetail, IonButton, IonContent, IonIcon, IonItem, IonLabel, IonList, IonModal, IonPopover } from "@ionic/react";
 import { close, closeCircle, closeCircleOutline, ellipsisHorizontal } from "ionicons/icons";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import './MoreOptions.css';
+import { createGesture, Gesture } from '@ionic/react';
+import { clamp } from "@ionic/core/dist/types/utils/helpers";
+import ShareModal from "./ShareModal";
 
-interface MoreOptionsProps {
-  isOwner:boolean;
+
+export const disableContentScrollY = (contentEl: HTMLElement): boolean => {
+
+  contentEl.style.setProperty('overflow', 'hidden');
+
+  return true;
 }
 
-const MoreOptions = (props:MoreOptionsProps) => {
-  const [isMenuOpen, setMenu] = useState(false);
+interface MoreOptionsProps {
+  isOwner?: boolean;
+  parentFunctionOne: Function;
+  parentFunctionTwo: Function;
+  isMenuOpen: boolean;
+  id:number;
+}
+
+const MoreOptions = (props: MoreOptionsProps) => {
 
   //added accessibility so menu is closed if it is active and the user hits the escape key.
   const escFunction = useCallback((event) => {
-    if (event.keyCode === 27) {   
-      setMenu(false); 
+    if (event.keyCode === 27) {
+      props.parentFunctionTwo(false);
     }
   }, []);
 
+  const handleMenuClick = () => {
+    //before opening the menu, set the id of the post we are opening it for
+    props.parentFunctionOne(props.id);
+    //open menu from the parent function
+    props.parentFunctionTwo(!props.isMenuOpen)
+  }
+
   useEffect(() => {
     //if menu is not open, do not add accessibility event listener for escape key
-    if (!isMenuOpen) {
+    if (!props.isMenuOpen) {
       return;
     }
-
     document.addEventListener("keydown", escFunction);
     return () => {
-        document.removeEventListener("keydown", escFunction);
+      document.removeEventListener("keydown", escFunction);
     };
-  }, [isMenuOpen]);
-
+  }, [props.isMenuOpen]);
 
   return (
-    <div className={isMenuOpen===true ? "nav-container active ":"nav-container "}>
-    <nav>
-        <ul className="options-nav">
-            <li className="flex">
-                <div className="menu-icon-container justify-end z-1-" onClick={() => setMenu(!isMenuOpen)} >
-                <div className={isMenuOpen ? "text-gray-800 opacity-60 transition" : "text-gray-700 transition"}>
+    <div className="menu-icon-container justify-end" onClick={() => props.parentFunctionTwo(!props.isMenuOpen)} >
+      <div className={props.isMenuOpen ? "text-gray-800 opacity-60 transition" : "text-gray-700 transition"}>
         <IonIcon slot="start" size='small' icon={ellipsisHorizontal} />
       </div>
-                </div>
-            </li>
-        </ul>
-        <ul className="active-nav">
-            <li>
-                <a>Edit</a>
-            </li>
-            <li>
-                <a>Insights</a>
-            </li>
-            <li>
-                <a>Delete</a>
-            </li>
-            <li>
-                <a>Share</a>
-            </li>
-            <div className="menu-icon-container mx-auto mt-36" onClick={() => setMenu(!isMenuOpen)} >
-                        <div className="menu-icon">
-                            <span className="line-1"></span>
-                            <span className="line-2"></span>
-                        </div>
-                    </div>
-        </ul>
-    </nav>
-</div>
+    </div>
   );
 };
 
