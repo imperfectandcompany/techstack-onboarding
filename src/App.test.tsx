ffb16c7b8679c9984863479121bf9cca50e19e37
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-06-12 15:51:54 -0400

    Finish compatibility for tests
    --

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react'
 import App from './App';
 
+afterEach(cleanup)
+
+describe('This will test my <App /> Component', () => {
 test('renders without crashing', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
 });
+})
+
