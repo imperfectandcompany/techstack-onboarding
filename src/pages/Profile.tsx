cd892123f213e536f50b3cd7c2ccf559b0442559
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-06-22 18:51:18 -0400

    foundation for profile page structure... low fidelity

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,10 +4,32 @@ import {
   IonContent,
   IonHeader,
   IonPage,
-  IonTitle,
   IonToolbar,
   ScrollDetail,
 } from '@ionic/react'
+
+
+interface profileProps {
+  username:string
+  followsMe:boolean
+  following:boolean
+  followerCount:number
+  followingCount:number
+  firstName:string
+  lastName:string
+}
+
+const profileData:profileProps = ()=> [
+{
+  username:'',
+  followsMe:true,
+  following:false,
+  followerCount:4,
+  followingCount:6,
+  firstName:'joe',
+  lastName:'mama'
+}
+]
 
 const Profile: React.FC = () => {
   let prevScrollpos = window.pageYOffset
@@ -25,23 +47,136 @@ const Profile: React.FC = () => {
     }
   }
 
+const uatww = profileData.firstName;
+  
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar color='none'>
-          <IonButtons slot='start'>
-            <IonBackButton text='' color='white' />
+          <IonButtons slot="start">
+            <IonBackButton color='dark' />
           </IonButtons>
-          <IonTitle color='white'>New Account</IonTitle>
+          <IonButtons slot="end">
+            Report
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent
         fullscreen={true}
-        color='dark'
+        color='light'
         scrollEvents={true}
         onIonScroll={(e) => onScroll(e)}
       >
-        ddd
+        <div className="flex items-center justify-evenly">
+          {/* profile pic */}
+          <div className="bg-gray-200 rounded-full w-16 h-16 ">
+          </div>
+          <div>
+            {/* username */}
+            <div className="flex gap-2">
+              <div>
+                Username
+              </div>
+              {/* relationship status chip */}
+              <div>
+                Follows You
+              </div>
+            </div>
+            {/* clout check */}
+            <div className="flex gap-2">
+              <div>
+                12 Followers
+              </div>
+              <div>
+                12 Following
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Description */}
+
+
+
+    {/* Name and Bio */}
+        <div>
+          <div>
+            Joe Mama
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div>
+          </div>
+        </div>
+        <div>
+
+          {/* row for interactions */}
+          <div className="flex justify-between">
+            <div className="flex gap-2">
+              <div >
+                heart
+              </div>
+              <div>
+                more options
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div >
+                message
+              </div>
+              <div>
+                follow
+              </div>
+            </div>
+          </div>
+{/* Header titles */}
+<div className="flex justify-around">
+<div>
+  Tab1
+</div>
+<div>
+  Tab1
+</div>
+<div>
+  Tab1
+</div>
+<div>
+  Tab1
+</div>
+</div>
+
+
+{/* Filter titles */}
+<div className="flex justify-around">
+<div>
+  Filter1
+</div>
+<div>
+  Filter2
+</div>
+<div>
+  Filter3
+</div>
+<div>
+  Filter4
+</div>
+</div>
+
+{/* Filter titles */}
+<div>
+<div>
+  Recent Activity
+</div>
+</div>
+
+
+
+
+
+
+        </div>
+
       </IonContent>
     </IonPage>
   )
